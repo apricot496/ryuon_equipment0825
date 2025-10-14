39c3776fd2aa47c4cc191bf3dd2770d0411becb3
Apricot-ds <t.mori@datumstudio.jp>
2025-10-14 11:49:16 +0900

    index_drop更新

--- index_drop_db.py
+++ index_drop_db.py
@@ -6,23 +6,26 @@ DB_PATH = "equipment.db"
 conn = sqlite3.connect(DB_PATH)
 cur = conn.cursor()
 
-cur.execute("""
-    CREATE TABLE new_eqipment_img_scraping AS
-    WITH 
-    max_num_index AS (
-        SELECT *
-        FROM eqipment_img_scraping
-        WHERE URL_Number = (SELECT MAX(URL_Number) FROM eqipment_img_scraping)
-    ),
-    re_eqipment_img_scraping AS (
-        SELECT *
-        FROM eqipment_img_scraping
-        WHERE 装備名 IS NOT NULL
-    )
+cur.execute("""CREATE TABLE new_eqipment_img_scraping AS
+WITH 
+max_num_index AS (
+    SELECT *
+    FROM eqipment_img_scraping
+    WHERE URL_Number = (SELECT MAX(URL_Number) FROM eqipment_img_scraping)
+),
+re_eqipment_img_scraping AS (
+    SELECT *
+    FROM eqipment_img_scraping
+    WHERE 装備名 IS NOT NULL
+),
+latest_numbered_equipment_table AS (
     SELECT * FROM re_eqipment_img_scraping
     UNION ALL
-    SELECT * FROM max_num_index WHERE 装備名 IS NULL
-""")
+    SELECT * FROM max_num_index
+)
+SELECT DISTINCT *
+FROM latest_numbered_equipment_table;
+            """)
 
 # 元テーブルを置き換えたい場合
 cur.execute("DROP TABLE eqipment_img_scraping")
