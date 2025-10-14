a48250912366753014ffca5c226a5bdc6006a151
Apricot-ds <t.mori@datumstudio.jp>
2025-10-14 11:58:50 +0900

    更新

--- reload_log.py
+++ reload_log.py
@@ -18,14 +18,35 @@ def load_log():
         st.error(f"データベース読み込みエラー: {e}")
         df = pd.DataFrame()
     return df
+def load_scraiping():
+    """スクレイピング結果の更新履歴を読み込み"""
+    try:
+        with sqlite3.connect("equipment.db") as conn:
+            # SQLiteでdatetime()に変換してORDER
+            query = 'SELECT * FROM eqipment_img_scraping ORDER BY URL_Number DESC;'
+            df = pd.read_sql(query, conn)
+            
+    except Exception as e:
+        st.error(f"データベース読み込みエラー: {e}")
+        df = pd.DataFrame()
+    return df
 
 def main():
-    st.title("更新履歴")
-    df = load_log()
-    if not df.empty:
-        st.dataframe(df, use_container_width=True)
-    else:
-        st.info("更新履歴が存在しません。")
+    log ,scraiping = st.tabs(["更新履歴", "スクレイピング結果"])
+    with log:
+        st.subheader("更新履歴")
+        df = load_log()
+        if not df.empty:
+            st.dataframe(df, use_container_width=True)
+        else:
+            st.info("更新履歴が存在しません。")
+    with scraiping:
+        st.subheader("スクレイピング結果")  
+        df = load_scraiping()
+        if not df.empty:
+            st.dataframe(df, use_container_width=True)
+        else:
+            st.info("更新履歴が存在しません。")
 
 if __name__ == "__main__":
     main()
