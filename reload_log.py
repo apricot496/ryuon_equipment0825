f426f46e747c8323c9ee87c3b92c4ca36518f17f
Apricot-ds <t.mori@datumstudio.jp>
2025-12-15 16:43:19 +0900

    テスト差分push

--- reload_log.py
+++ reload_log.py
@@ -31,8 +31,53 @@ def load_scraiping():
         df = pd.DataFrame()
     return df
 
+def load_equipments_data():
+    """SQLite DB からデータを読み込む"""
+    conn = sqlite3.connect("equipment.db")
+    # スプレッドシートに記載済みのdataを取得
+    equipments_list = ["武器", "防具", "装飾"]
+    rarelity_order = ['ur', 'ksr', 'ssr']
+    df_list = []
+    for equipments in equipments_list:
+        equipment_df_concat_list = []
+        for  rarelity in rarelity_order:
+            sheet_name = f"{rarelity}{equipments}"
+            df = pd.read_sql(f"""SELECT 
+    装備名
+    , 装備番号
+    , レアリティ
+    , 体力
+    , 攻撃力
+    , 防御力
+    , 会心率
+    , 命中率
+    , 回避率
+    , アビリティ
+    , アビリティカテゴリ
+FROM '{sheet_name}' """, conn)
+            df_list.append(df)
+    df = pd.concat(df_list, ignore_index=True)
+    df = df.replace('', pd.NA)
+    df['アビリティカテゴリ'] = df['アビリティカテゴリ'].fillna('アビリティなし')
+    # スクレイピング結果を取得
+    df_scraping = pd.read_sql("""SELECT
+                              装備名
+                            , レアリティ
+                            ,BASE64
+                              FROM equipment_img_scraping
+                              """, conn)
+    diff_df = pd.merge(df, df_scraping, on=['装備名', 'レアリティ'], how='right', indicator=True)
+    diff_df = diff_df[diff_df['_merge'] != 'both'].drop(columns=['_merge'])
+    # # アビリティカテゴリ
+    # df_category = pd.read_sql("SELECT * FROM 'ability_category'", conn)
+    # df_nan = pd.DataFrame({'アビリティカテゴリ分類': ['アビリティなし']})
+    # df_category = pd.concat([df_category, df_nan])
+
+    conn.close()
+    return diff_df
+
 def main():
-    log ,scraiping = st.tabs(["更新履歴", "スクレイピング結果"])
+    log ,scraiping, diff = st.tabs(["更新履歴", "スクレイピング結果","スクレイピング結果の差分"])
     with log:
         st.subheader("更新履歴")
         df = load_log()
@@ -47,6 +92,11 @@ def main():
             st.dataframe(df, use_container_width=True)
         else:
             st.info("更新履歴が存在しません。")
+    with diff:
+        st.subheader("スクレイピング結果の差分")  
+        diff_df = load_equipments_data()
+        st.dataframe(diff_df, use_container_width=True)
+
 
 if __name__ == "__main__":
     main()
